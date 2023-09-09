d803ad76de9f6a8119ce611530f25894a43f9c77
Svetoslav Lyubenov <patchino76@gmail.com>
2023-09-09 14:05:23 +0300

    3-5 Conditional rendering - finished

--- react_app/src/components/ListGroup.tsx
+++ react_app/src/components/ListGroup.tsx
@@ -1,12 +1,20 @@
 function ListGroup() {
-  const cities = ["София", "Пловдив", "Варна", "Бургас", "Земен"];
+  let cities = ["София", "Пловдив", "Варна", "Бургас", "Земен"];
+  // cities = [];
+
+  // if (cities.length == 0) return <p>No items</p>;
+  
+  // const getMessage = () => {
+  //   cities.length == 0 ? <p>No items</p> : null;
+  // };
 
   return (
     <>
       <h1>List</h1>
+      {DataTransferItemList.length == 0 && <p>No items</p>}
       <ul className="list-group">
         {cities.map((city) => (
-          <li>{city}</li>
+          <li key={city}>{city}</li>
         ))}
       </ul>
     </>
