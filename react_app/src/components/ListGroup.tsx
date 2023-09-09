bab41afce1f5d4941d00c9873ca91c971ec7076a
Svetoslav Lyubenov <patchino76@gmail.com>
2023-09-09 16:19:08 +0300

    3-6 Passing data via props

--- react_app/src/components/ListGroup.tsx
+++ react_app/src/components/ListGroup.tsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
+
 function ListGroup() {
   let cities = ["София", "Пловдив", "Варна", "Бургас", "Земен"];
   // cities = [];
 
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   // if (cities.length == 0) return <p>No items</p>;
-  
+
   // const getMessage = () => {
   //   cities.length == 0 ? <p>No items</p> : null;
   // };
 
+  // const handleClick = (e:MouseEvent) => {console.log(e)}
   return (
     <>
       <h1>List</h1>
       {DataTransferItemList.length == 0 && <p>No items</p>}
       <ul className="list-group">
-        {cities.map((city) => (
-          <li key={city}>{city}</li>
+        {cities.map((city, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={city}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {city}
+          </li>
         ))}
       </ul>
     </>
